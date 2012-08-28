6070a980729c79a6832b1ca94c02cc80be4a2f7b
Shubham Gupta <shubham.gupta@vinsol.com>
2012-08-28 19:41:11 +0530

    Update Ex4.rb

--- Ex4.rb
+++ Ex4.rb
@@ -1,11 +1,10 @@
-puts "Please enter a string.(Press q/Q to exit)\n"
-str = gets.chomp.downcase
-while str.downcase!='q'
-if str==str.reverse
-  puts "It's a pallindrome" 
-else
-  puts "Not a pallindrome"
-end
-puts "Please enter a string.(Press q/Q to exit)\n"
-str = gets.chomp.downcase
+loop do
+  puts "Please enter a string.(Press q/Q to exit)\n"
+  str = gets.chomp.downcase
+  break if str ==  'q'
+  if  str==str.reverse
+    puts "It's a pallindrome"
+  else
+    puts "Not a pallindrome"
+  end
 end
