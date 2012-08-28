7d1710a10ae4e8a3f809c0ce51b2fbb2b97485aa
Shubham Gupta <shubham.gupta@vinsol.com>
2012-08-28 12:56:01 +0530

    Update Ex1.rb

--- Ex1.rb
+++ Ex1.rb
@@ -1,11 +1,7 @@
 puts "Please enter a string.\n"
 str = gets.chomp.downcase
 char_hash=Hash.new(0) #Initially empty hash
-str.each_char {|c| 
-  if char_hash[c]
-    char_hash[c] = char_hash[c]+1
-  else
-    char_hash[c]=1
-  end
-}
+str.each_char do |c| 
+  char_hash[c] += 1
+end
 puts char_hash
